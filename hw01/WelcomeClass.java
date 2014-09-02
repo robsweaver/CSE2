3b9fc32dc3cba7d5b57a91f4219707b45568ff7d
robsweaver <rsw218@lehigh.edu>
2014-09-02 00:01:22 +0000

    finished hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,24 @@
+//Rob Weaver
+//1 September 2014
+//Welcome Class Java Program
+
+//  define a class
+    public class WelcomeClass{
+
+//      add main method
+        public static void main(String[] args) {
+            
+//          print text
+            System.out.println("    -----------      ");
+            System.out.println("    | WELCOME |      ");
+            System.out.println("    -----------      ");
+            System.out.println("  ^  ^  ^  ^  ^  ^ ");
+            System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+            System.out.println("|-R--S--W--2--1--8-|");
+            System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+            System.out.println("  v  v  v  v  v  v");
+            System.out.println("I went to Australia for 3 weeks.");
+            
+        }
+    }
+ 
