184e428db9b95636b9752eccddaa4c5bb9f963dc
robsweaver <rsw218@lehigh.edu>
2014-09-17 16:55:47 +0000

    Four Digits

--- hw03/FourDigits.java
+++ hw03/FourDigits.java
@@ -3,7 +3,7 @@
 // CSE hw03
 // Root Java Program
 
-//  create a scanner to accept user input
+//  import scanner to accept user input
 import java.util.Scanner;
 
 //  define a class
@@ -12,20 +12,20 @@ import java.util.Scanner;
 //      create main method
         public static void main(String[] args) {
             
-        Scanner myScanner;
-        myScanner = new Scanner( System.in );    
+            Scanner myScanner;  //create scanner
+            myScanner = new Scanner( System.in );    
             
-        System.out.print("Enter a number with 4 decimals");
-        double entry = myScanner.nextDouble();
+            System.out.print("Enter a number with 4 decimals:" );   //prompt user for a number with 4 decimals
+            double entry = myScanner.nextDouble();  //declare 'entry' as the name of the double
         
-        double doubledigits = entry*10000;
+            double doubledigits = entry*10000; //declare doubledigits variable as user input*10000
         
-        int digit1=(int)((doubledigits % 10000)/1000);
-        int digit2=(int)(((entry*10000) = (digit1*1000) - ((int)entry*10000))/100); 
-        int digit3=(int)((((entry*10000) - (digit1*1000) - (digit2*100) - ((int)entry*10000))/10));
-        int digit4=(int)((((entry*10000) - (digit1*1000) - (digit2*100) - (digit3*10) - ((int)entry*10000))/1));
+            int digit1=(int)((doubledigits % 10000)/1000);  //calculation to isolate the first digit
+            int digit2=(int)(((entry*10000) - (digit1*1000) - ((int)entry*10000))/100); //isolate second digit
+            int digit3=(int)((((entry*10000) - (digit1*1000) - (digit2*100) - ((int)entry*10000))/10)); //isolate third digit
+            int digit4=(int)((((entry*10000) - (digit1*1000) - (digit2*100) - (digit3*10) - ((int)entry*10000))/1));    //isolate fourth digit
         
-        System.out.println("The four digits after the decimal place are "+digit1+digit2+digit3+digit4);
+            System.out.println("The four digits after the decimal place are "+digit1+digit2+digit3+digit4); //print with all digits
         
         }
     }        
